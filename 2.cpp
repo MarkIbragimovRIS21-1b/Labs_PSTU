7229ef892f07f0f9259fb012ef10b30e43579968
MarkIbragimovRIS21-1b <92600879+MarkIbragimovRIS21-1b@users.noreply.github.com>
2021-10-30 19:48:46 +0500

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,35 +1,24 @@
 #include <iostream>
-#include <math.h>
-using namespace std;
-int main()
-{
-int a = 0, b = 0, c = 0, x = 0;
-bool F = false;
-cout << "Введите трёхзначное число: ";
-#include <iostream>
 #include <ctime>
-
 using namespace std;
-
 int main()
 {
-    srand(time(NULL));
-    int a = rand() % 99 + 1, x;
-    cout << "У вас есть 6 попыток угадать число от 1 до 99: ";
-    for (int i = 5; i > -1; i--) {
-        cin >> x;
-        if (x == a) {
-            cout << "Вы угадали число!";
-            break;
-        }
-        else
-            if (i > 0)
-                if (a > x) cout << "Загаданное число больше " << x << endl;
-                else if (a < x) cout << "Загаданное число меньше " << x << endl;
-                cout << "Попыток осталось: " << i << endl << "Введите число: ";
-        if (i == 0)
-            cout << "Попытки кончились";
-    }
-    return 0;
-
+srand(time(NULL));
+int k = rand() % 99 + 1, x;
+cout << "У вас есть 6 попыток угадать число от 1 до 99: ";
+for (int i = 5; i > -1; i--) {
+cin >> x;
+if (x == k) {
+cout << "Вы угадали число!";
+break;
+}
+else
+if (i > 0)
+if (k > x) cout << "Загаданное число больше " << x << endl;
+else if (k < x) cout << "Загаданное число меньше " << x << endl;
+cout << "Попыток осталось: " << i << endl << "Введите число: ";
+if (i == 0)
+cout << "Попытки кончились";
+}
+return 0;
 }
