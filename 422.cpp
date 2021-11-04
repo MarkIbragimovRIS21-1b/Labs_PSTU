70c7a4f2781806d26045add1e9e99b4a3f08bd51
MarkIbragimovRIS21-1b <92600879+MarkIbragimovRIS21-1b@users.noreply.github.com>
2021-11-04 18:12:17 +0500

    Create 422.cpp

---
+++ 422.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int i;
+cout << "Введите число галлонов: " << endl;
+cin >> i;
+cout << "Это получится: " << i / 7.481 << " кубических футов" << endl;
+return 0;
+}
