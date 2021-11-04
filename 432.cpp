499efdc734574d089f4b401ab27a97bcf5d279db
MarkIbragimovRIS21-1b <92600879+MarkIbragimovRIS21-1b@users.noreply.github.com>
2021-11-04 18:14:58 +0500

    Create 432.cpp

---
+++ 432.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+int main()
+{
+SetConsoleCP(1251);
+SetConsoleOutputCP(1251);
+char a, b, c, d, e;
+cin >> a >> b >> c >> d >> e;
+cout << e << d << c << b << a;
+return 0;
+}
